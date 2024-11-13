c81c819bbd1fe80d8557f960aa755b8ed1be3076
yanyiik99 <aripramana574@gmail.com>
2024-11-13 22:02:19 +0800

    update: home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,42 @@
+import { TypeAnimation } from 'react-type-animation';
+import './index.css';
+import { Row, Col, Button, Flex } from 'antd';
+import { ArrowRightOutlined} from '@ant-design/icons';
+
 
 const Home = () => {
   return (
-    <div>
-        <h1 className="text-red-400">Home</h1>
+    <div className="bg-homepage">
+        <div className="backdrop"></div>
+        <Row className='h-screen' justify="center" align="middle">
+            <Col span={12}>
+                <Flex align='center' justify='center' vertical>
+                    <TypeAnimation
+                        sequence={[
+                            'One',
+                            500,
+                            'Thre Two',
+                            500,
+                            'One Two Three',
+                            500,
+                            'One Two',
+                            500,
+                            'One',
+                            500,
+                            '',
+                            500,
+                        ]}
+                        style={{ fontSize: '3rem' }}
+                        className='text-white font-bold'
+                        repeat={Infinity}
+                    />
+                    <Button size='large' className='my-4' type="primary" iconPosition='end' icon={<ArrowRightOutlined />}>
+                        Playlist NPM INSTALL
+                    </Button>
+                </Flex>
+            </Col>
+        </Row>
+
     </div>
   )
 }
